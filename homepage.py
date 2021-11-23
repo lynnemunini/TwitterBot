4216065fa5a8425dabc48e12ac96639b95dfea5b
Lynne Munini <muninilynne65@gmail.com>
2021-11-22 23:42:25 -0500

    Twitter Playing Mind Tricks on me

--- homepage.py
+++ homepage.py
@@ -8,14 +8,13 @@ class HomePage:
         self.driver = driver
         self.driver.get('https://twitter.com/')
         time.sleep(5)
-        sign_in = self.driver.find_element(By.XPATH, "//*[@id='react-root']/div/div/div/main/div/div/div/div[1]/"
-                                                     "div/div[3]/div[4]/span/span")
+        sign_in = self.driver.find_element(By.XPATH, "//a[@data-testid='loginButton']")
         sign_in.click()
         time.sleep(5)
-        sign_in_name = self.driver.find_element(By.XPATH,
-                                           "//*[@id='react-root']/div/div/div/main/div/div/div/div[1]/div/div[3]/a/div/span/span")
-        sign_in_name.click()
-        time.sleep(5)
+        # sign_in_name = self.driver.find_element(By.XPATH,
+        #                                    "//*[@id='react-root']/div/div/div/main/div/div/div/div[1]/div/div[3]/a/div/span/span")
+        # sign_in_name.click()
+        # time.sleep(5)
 
     def go_to_login_page(self):
         return LoginPage(self.driver)
